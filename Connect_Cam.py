ab14c3138f4746fa7ccbe2ac75eb46145bd46761
Mathieu Vigneault <mathieu.vigneault.19@gmail.com>
2020-08-29 10:40:16 -0400

    help

--- Connect_Cam.py
+++ Connect_Cam.py
@@ -1,18 +1,68 @@
+from matplotlib import pyplot
+from PIL import Image
+from numpy import asarray
+from scipy.spatial.distance import cosine
+from mtcnn.mtcnn import MTCNN
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
 import cv2
 
-cv2.namedWindow("preview")
+
+# extract a single face from a given photograph
+def extract_face(filename, required_size=(224, 224)):
+    # load image from file
+    pixels = pyplot.imread(filename)
+    # create the detector, using default weights
+    detector = MTCNN()
+    # detect faces in the image
+    results = detector.detect_faces(pixels)
+    # extract the bounding box from the first face
+    x1, y1, width, height = results[0]['box']
+    x2, y2 = x1 + width, y1 + height
+    # extract the face
+    face = pixels[y1:y2, x1:x2]
+    # resize pixels to the model size
+    image = Image.fromarray(face)
+    image = image.resize(required_size)
+    face_array = asarray(image)
+    return face_array
+
+
+# extract faces and calculate face embeddings for a list of photo files
+def get_embeddings(filenames):
+    # extract faces
+    faces = [extract_face(f) for f in filenames]
+    # convert into an array of samples
+    samples = asarray(faces, 'float32')
+    # prepare the face for the model, e.g. center pixels
+    samples = preprocess_input(samples, version=2)
+    # create a vggface model
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+    # perform prediction
+    yhat = model.predict(samples)
+    return yhat
+
+
+# determine if a candidate face is a match for a known face
+def is_match(known_embedding, candidate_embedding, thresh=0.5):
+    # calculate distance between embeddings
+    score = cosine(known_embedding, candidate_embedding)
+    if score <= thresh:
+        print('>face is a Match (%.3f <= %.3f)' % (score, thresh))
+    else:
+        print('>face is NOT a Match (%.3f > %.3f)' % (score, thresh))
+
+
+# define filenames
+filenames = ['Mat.jpg']
+# get embeddings file filenames
+embeddings = get_embeddings(filenames)
+# define sharon stone
+Mat_id = embeddings[0]
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened():
-    rval, frame = vc.read()
-else:
-    rval = False
-
-for i in range(300):
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27:
-        break
-vc.release()
-cv2.destroyWindow("preview")
+rval, frame = vc.read()
+frameID = get_embeddings(pyplot.imread(frame))
+# verify known photos of sharon
+print('Positive Tests')
+is_match(Mat_id, frameID)
